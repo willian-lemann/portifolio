9f5140ae39b99d67a7beb85430d8aabc4538f4b2
Willian Lemann <willianleman@hotmail.com>
2023-04-10 18:50:29 -0300

    feat: resume open link pdf

--- src/app/Navigation.tsx
+++ src/app/Navigation.tsx
@@ -3,6 +3,7 @@
 import { useDarkMode } from "@/stores/dark";
 import { classnames } from "@/utils/classnames";
 import { ChevronDownIcon, MoonIcon, SunIcon } from "@heroicons/react/24/solid";
+
 import Image from "next/image";
 
 type NavigationProps = {
@@ -12,11 +13,20 @@ type NavigationProps = {
 export function Navigation({ actionLabel }: NavigationProps) {
   const { toggle, darkMode } = useDarkMode((state) => state);
 
+  function handleOpen() {
+    window.open("/willian-lemann-profile.pdf");
+  }
+
   return (
     <nav className="py-10 mb-12 flex justify-between items-center dark:text-white">
       <div className="flex items-center">
         <div className="relative h-10 w-10 rounded-full">
-          <Image src="/avatar.jpeg" alt="avatar image" fill className="object-cover rounded-full" />
+          <Image
+            src="/avatar.jpeg"
+            alt="avatar image"
+            fill
+            className="object-cover rounded-full"
+          />
         </div>
         <h1 className="font-poppins text-xl pl-3">Willian Lemann</h1>
       </div>
@@ -55,6 +65,7 @@ export function Navigation({ actionLabel }: NavigationProps) {
           <a
             className="bg-gradient-to-r from-cyan-500 text- to-teal-500 text-white px-4 py-2 border-none rounded-md ml-8"
             href="#"
+            onClick={handleOpen}
           >
             {actionLabel}
           </a>
